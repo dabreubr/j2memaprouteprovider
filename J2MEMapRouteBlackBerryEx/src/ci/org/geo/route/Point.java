2686fa87f1ac980456afe4f77ca6576ec85df7e5
max.gontar <max.gontar@8698076a-fb98-11de-835b-cde17aa922c3>
2010-01-07 14:47:30 +0000

    initial commit

---
+++ J2MEMapRouteBlackBerryEx/src/ci/org/geo/route/Point.java
@@ -0,0 +1,9 @@
+package ci.org.geo.route;
+
+public class Point {
+	String mName;
+	String mDescription;
+	String mIconUrl;
+	double mLatitude;
+	double mLongitude;
+}
---
+++ J2MEMapRouteBlackBerryEx/src/ci/org/geo/route/Road.java
@@ -0,0 +1,10 @@
+package ci.org.geo.route;
+
+public class Road {
+	public String mName;
+	public String mDescription;
+	public int mColor;
+	public int mWidth;
+	public double[][] mRoute = new double[][] {};
+	public Point[] mPoints = new Point[] {};
+}
