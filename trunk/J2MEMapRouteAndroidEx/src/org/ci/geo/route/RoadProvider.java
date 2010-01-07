b8be626ea013199f499bb8d62a4279cd009bb825
max.gontar <max.gontar@8698076a-fb98-11de-835b-cde17aa922c3>
2010-01-07 20:56:38 +0000

    minor

--- trunk/J2MEMapRouteAndroidEx/src/org/ci/geo/route/RoadProvider.java
+++ trunk/J2MEMapRouteAndroidEx/src/org/ci/geo/route/RoadProvider.java
@@ -12,8 +12,6 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import android.util.Log;
-
 public class RoadProvider {
 
 	public static Road getRoute(InputStream is) {
